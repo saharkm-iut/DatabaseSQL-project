481318c74392612fdb9bae6c0e8344afe282919f
zhrA006 <zahra.a00200206@gmail.com>
2026-07-09 23:50:24 +0330

    persiciors for insert into a table that will need

---
+++ taxipresude.sql
@@ -0,0 +1,41 @@
+------------taxiprocedures
+
+
+create procedure Taxi.CreateDriver
+(
+    @firstname NVARCHAR(50),
+    @lastname NVARCHAR(50),
+    @phone_num VARCHAR(11),
+    @email VARCHAR(100),
+    @pass_hash VARCHAR(255),
+
+    @license_num VARCHAR(10),
+    @national_code VARCHAR(10)
+)
+
+
+as begin
+begin transaction;
+begin try
+
+DECLARE @user_id INT;
+DECLARE @driver_role INT;
+INSERT INTO Account.users(firstname,lastname,phone_num, email,pass_hash)
+VALUES(@firstname,@lastname, @phone_num, @email, @pass_hash );
+SET @user_id = SCOPE_IDENTITY();
+
+SELECT @driver_role = role_id
+FROM Account.roles
+WHERE role_name='Driver';
+INSERT INTO Account.user_roles(user_id,role_id)
+VALUES(@user_id,@driver_role);
+INSERT INTO Taxi.drivers(driver_id,license_num,national_code,is_approved,is_online)
+VALUES(@user_id,@license_num,@national_code,1,0);
+COMMIT;
+SELECT 'Driver Created' AS Message, @user_id AS UserID;
+END TRY
+BEGIN CATCH
+ROLLBACK;
+THROW;
+END CATCH
+END;
